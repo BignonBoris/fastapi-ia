5ab47d4ebf304498ece2e21baf75bfca7fb92182
Bignon Boris <bignonboris@gmail.com>
2025-08-06 15:43:23 +0200

    mise à jour du cors

--- cors.py
+++ cors.py
@@ -2,13 +2,14 @@ from fastapi.middleware.cors import CORSMiddleware
 
 def setup_cors(app):
     origins = [
+        "https://projet-ia-osi4.vercel.app",  # frontend en ligne sur vercel
         "http://localhost:3000",  # frontend local
-        "https://projet-ia-osi4.vercel.app/",  # frontend en ligne sur vercel
         # ajoute ici les autres origines si besoin (ex : déploiement)
     ]
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=origins,           # Liste des domaines autorisés
+        allow_origins=["*"],  # ⚠ Ouvre à tout le monde
+        # allow_origins=origins,           # Liste des domaines autorisés
         allow_credentials=True,
         allow_methods=["*"],             # GET, POST, etc.
         allow_headers=["*"],             # Autorise tous les headers
